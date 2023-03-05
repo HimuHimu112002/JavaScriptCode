855f238b38aa63b1cf890ed70c716b4a066eef68
MD Aktarujjaman <82214871+HimuHimu112002@users.noreply.github.com>
2023-03-05 22:49:44 +0600

    Add files via upload

---
+++ JS/para.js
@@ -0,0 +1,11 @@
+let a = 100
+let b = 200
+let c = 30
+
+if(a>b && a>c){
+    console.log("A is bigger")
+}else if(b>a && b>c){
+    console.log("B is bigger")
+}else{
+    console.log("C is bigger")
+}
